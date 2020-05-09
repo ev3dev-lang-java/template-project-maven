d881535a674c9a2b23bcd778579c48ed972dd359
Juan Antonio Breña Moral <bren@juanantonio.info>
2020-05-09 17:20:06 +0200

    Initial maven solution to deploy on EV3

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # template-project-maven
 A ready to use ev3dev-lang-java project to help users with the task to begin a new project with Maven from Scratch
+
+# How to use
+
+```
+mvn -Pbrick clean package
+```
---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+echo "#####################"
+echo "# MAVEN EV3 SCRIPTS #"
+echo "#####################"
+echo
+
+EV3_HOST=$1
+EV3_USER=$2
+EV3_PASSWORD=$3
+
+ARTIFACT=$4
+
+#echo $EV3_HOST
+#echo $EV3_USER
+#echo $EV3_PASSWORD
+#echo $ARTIFACT
+
+function deploy() {
+
+    echo "Deploy artifact to EV3"
+    echo "sshpass -p $EV3_PASSWORD scp -P 22 ./target/$ARTIFACT.jar $EV3_USER@$EV3_HOST:/home/robot/"
+    sshpass -p $EV3_PASSWORD \
+        scp -P 22 \
+        ./target/$ARTIFACT.jar $EV3_USER@$EV3_HOST:/home/robot/
+}
+
+function run() {
+
+    echo "Run artifact"
+    echo "sshpass -p $EV3_PASSWORD ssh -p 22 $EV3_USER@$EV3_HOST java -jar $ARTIFACT.jar"
+    sshpass -p $EV3_PASSWORD \
+        ssh -p 22 \
+        $EV3_USER@$EV3_HOST "java -jar $ARTIFACT.jar"
+}
+
+deploy
+run
